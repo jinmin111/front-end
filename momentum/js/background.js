21b0fa709c9b583f34d291e7337dc673f6ee67aa
jinmin111 <ramyeon00@gmail.com>
2022-02-19 19:57:25 +0900

    Update background.js

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,11 +1,20 @@
 const images = [
-    "0.jpeg",
-    "1.jpeg",
-    "2.jpeg"
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
 ];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url("img/${chosenImage}")`
+document.body.style.backgroundSize="cover"
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImage}`;
+// document.body.appendChild(bgImage);
 
